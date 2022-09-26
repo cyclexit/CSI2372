6030641d8f933022169aaab74246a53ded7d5bb4
cyclexit <cyclexit@outlook.com>
2022-09-26 14:23:05 -0400

    Finish Relation.h

--- lab/2/Relation.h
+++ lab/2/Relation.h
@@ -6,7 +6,43 @@
 #include <utility>
 
 class Relation {
+ public:
+  // constants
 
+  // ctors and dtors
+  Relation();
+  Relation(const Relation& other);
+  ~Relation() = default;
+
+  // methods
+  size_t cardinality() const;
+
+  bool add_element(std::pair<int, int> r);
+
+  void remove_element(std::pair<int, int> r);
+
+  bool is_member(std::pair<int, int> r) const;
+
+  bool equal(const Relation& other) const;
+
+  bool reflexive() const;
+
+  bool irreflexive() const;
+
+  bool symmetric() const;
+
+  bool asymmetric() const;
+
+  bool transitive() const;
+
+  bool is_function() const;
+
+  Relation inverse() const;
+
+  Relation combination(const Relation& other) const;
+
+ private:
+  std::vector<std::pair<int, int>> relations_;
 };
 
 #endif // RELATION_H_
