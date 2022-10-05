c6863f19e344c9bb2c27ab8f001c9b91ac95d90b
cyclexit <cyclexit@outlook.com>
2022-10-05 13:20:49 -0400

    Add private data members

---
+++ assignment/1/BigInteger.h
@@ -0,0 +1,13 @@
+#ifndef CSI2371_ASSIGNMENT_1_BIG_INTEGER_H_
+#define CSI2371_ASSIGNMENT_1_BIG_INTEGER_H_
+
+class BigInteger {
+ public:
+
+ private:
+  char* digits_;
+  int len_;
+  int base_;
+};
+
+#endif // CSI2371_ASSIGNMENT_1_BIG_INTEGER_H_
