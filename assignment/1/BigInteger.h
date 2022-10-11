cfc93c3391a40f2a7640925e1a1363edb615f5d1
cyclexit <cyclexit@outlook.com>
2022-10-11 09:43:58 -0400

    Add the declaration of 3 operator+

--- assignment/1/BigInteger.h
+++ assignment/1/BigInteger.h
@@ -29,6 +29,9 @@ class BigInteger {
   bool operator!=(const BigInteger& other);
   // arithmetic operators
   BigInteger& operator=(const BigInteger& other);
+  BigInteger& operator+(int num);
+  friend BigInteger& operator+(int num, const BigInteger& big_num);
+  BigInteger& operator+(const BigInteger& other);
 
  private:
   // data member
