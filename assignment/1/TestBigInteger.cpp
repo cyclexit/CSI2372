ef11dd09f4b04b91160943b2ffde393c82532971
cyclexit <cyclexit@outlook.com>
2022-10-12 22:51:09 -0400

    Add some test cases

--- assignment/1/TestBigInteger.cpp
+++ assignment/1/TestBigInteger.cpp
@@ -20,9 +20,31 @@ int main() {
   cout << (b1 * -1) << endl;
 
   BigInteger b2(99, 10), b3(1000, 10);
-  cout << (b2 < b3) << endl;
-  cout << (b2 > b3) << endl;
-  cout << (b2 == b3) << endl;
+  cout << b2 << " " << b3 << endl;
+  printf("(b2 < b3) = %d\n", (b2 < b3));
+  printf("(b2 <= b3) = %d\n", (b2 <= b3));
+  printf("(b2 > b3) = %d\n", (b2 > b3));
+  printf("(b2 >= b3) = %d\n", (b2 >= b3));
+  printf("(b2 == b3) = %d\n", (b2 == b3));
+  printf("(b2 != b3) = %d\n", (b2 != b3));
+
+  b2 = 1000;
+  cout << b2 << " " << b3 << endl;
+  printf("(b2 < b3) = %d\n", (b2 < b3));
+  printf("(b2 <= b3) = %d\n", (b2 <= b3));
+  printf("(b2 > b3) = %d\n", (b2 > b3));
+  printf("(b2 >= b3) = %d\n", (b2 >= b3));
+  printf("(b2 == b3) = %d\n", (b2 == b3));
+  printf("(b2 != b3) = %d\n", (b2 != b3));
+
+  b2 = 9999;
+  cout << b2 << " " << b3 << endl;
+  printf("(b2 < b3) = %d\n", (b2 < b3));
+  printf("(b2 <= b3) = %d\n", (b2 <= b3));
+  printf("(b2 > b3) = %d\n", (b2 > b3));
+  printf("(b2 >= b3) = %d\n", (b2 >= b3));
+  printf("(b2 == b3) = %d\n", (b2 == b3));
+  printf("(b2 != b3) = %d\n", (b2 != b3));
 
   return 0;
 }
