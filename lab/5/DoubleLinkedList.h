68d5acb9aa087c96142b5cb801f68e3126f0efd6
cyclexit <cyclexit@outlook.com>
2022-10-18 10:40:25 -0400

    Finish the design of the header

--- lab/5/DoubleLinkedList.h
+++ lab/5/DoubleLinkedList.h
@@ -1,6 +1,38 @@
 #ifndef LAB_5_DOUBLE_LINKED_LIST_H_
 #define LAB_5_DOUBLE_LINKED_LIST_H_
 
+#include <iostream>
 
+class DoubleLinkedList {
+ public:
+  class Node {
+    int data;
+    Node* prev;
+    Node* next;
+  };
+
+  // ctors and dtors
+  DoubleLinkedList();
+  ~DoubleLinkedList();
+
+  // methods
+  bool add_to_front(int data);
+	bool add_to_back(int data);
+  bool insert_item(int idx, int data);
+  void remove_item(int data);
+  void remove_from_front();
+	void remove_from_back();
+
+  // operators
+  int operator[](int idx);
+  friend std::ostream& operator<<(std::ostream& out,
+                                  const DoubleLinkedList& lst);
+
+ private:
+  Node* head;
+  Node* tail;
+};
+
+using Node = DoubleLinkedList::Node;
 
 #endif // LAB_5_DOUBLE_LINKED_LIST_H_
