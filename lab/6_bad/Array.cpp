12ef778fb2379013ad2b09f0eacac61c4ebd2340
cyclexit <cyclexit@outlook.com>
2022-11-03 15:12:42 -0400

    Separate

--- lab/6_bad/Array.cpp
+++ lab/6_bad/Array.cpp
@@ -1,2 +1,80 @@
 #include "Array.h"
 
+template<typename T>
+Array<T>::Array() {
+  lower_ = 0;
+  upper_ = 0;
+}
+
+template<typename T>
+Array<T>::Array(int n) {
+  lower_ = 0;
+  upper_ = n - 1;
+  arr_.resize(n);
+}
+
+template<typename T>
+Array<T>::Array(const Array<T>& other) {
+  lower_ = other.lower_;
+  upper_ = other.upper_;
+  arr_ = other.arr_;
+}
+
+template<typename T>
+Array<T>::~Array() {
+  lower_ = 0;
+  upper_ = 0;
+}
+
+template<typename T>
+bool Array<T>::add_item(T elem) {
+  arr_.push_back(elem);
+  ++upper_;
+  return true;
+}
+
+template<typename T>
+bool Array<T>::insert_item(int idx, T elem) {
+  if (!is_index_valid(idx)) return false;
+  idx -= lower_;
+  arr_.insert(arr_.begin() + idx, elem);
+  ++upper_;
+  return true;
+}
+
+template<typename T>
+void Array<T>::remove_item() {
+  if (!arr_.empty()) arr_.pop_back();
+  if (upper_ > lower_) --upper_;
+}
+
+template<typename T>
+void Array<T>::remove_item(T elem) {
+  int idx = -1;
+  if (find_item(elem, idx)) {
+    idx -= lower_;
+    arr_.erase(arr_.begin() + idx);
+    --upper_;
+  }
+}
+
+template<typename T>
+void Array<T>::erase_item(int idx) {
+  if (is_index_valid(idx)) {
+    idx -= lower_;
+    arr_.erase(arr_.begin() + idx);
+    --upper_;
+  }
+}
+
+template<typename T>
+bool Array<T>::find_item(T elem, int& idx) {
+  for (int i = 0; i < arr_.size(); ++i) {
+    if (arr_[i] == elem) {
+      idx = lower_ + i;
+      return true;
+    }
+  }
+  idx = -1;
+  return false;
+}
--- lab/6_bad/Array.h
+++ lab/6_bad/Array.h
@@ -8,81 +8,19 @@ template<typename T>
 class Array {
  public:
   // ctors and dtors
-  Array() {
-    lower_ = 0;
-    upper_ = 0;
-  }
-
-  Array(int n) {
-    lower_ = 0;
-    upper_ = n - 1;
-    arr_.resize(n);
-  }
-
-  Array(int lower, int upper) {
-    lower_ = lower;
-    upper_ = upper;
-    arr_.resize(upper - lower + 1);
-  }
-
-  Array(const Array<T>& other) {
-    lower_ = other.lower_;
-    upper_ = other.upper_;
-    arr_ = other.arr_;
-  }
-
-  ~Array() {
-    lower_ = 0;
-    upper_ = 0;
-  }
+  Array();
+  Array(int n);
+  Array(int lower, int upper);
+  Array(const Array<T>& other);
+  ~Array();
 
   // methods
-  bool add_item(T elem) {
-    arr_.push_back(elem);
-    ++upper_;
-    return true;
-  }
-
-  bool insert_item(int idx, T elem) {
-    if (!is_index_valid(idx)) return false;
-    idx -= lower_;
-    arr_.insert(arr_.begin() + idx, elem);
-    ++upper_;
-    return true;
-  }
-
-  void remove_item() {
-    if (!arr_.empty()) arr_.pop_back();
-    if (upper_ > lower_) --upper_;
-  }
-
-  void remove_item(T elem) {
-    int idx = -1;
-    if (find_item(elem, idx)) {
-      idx -= lower_;
-      arr_.erase(arr_.begin() + idx);
-      --upper_;
-    }
-  }
-
-  void erase_item(int idx) {
-    if (is_index_valid(idx)) {
-      idx -= lower_;
-      arr_.erase(arr_.begin() + idx);
-      --upper_;
-    }
-  }
-
-  bool find_item(T elem, int& idx) {
-    for (int i = 0; i < arr_.size(); ++i) {
-      if (arr_[i] == elem) {
-        idx = lower_ + i;
-        return true;
-      }
-    }
-    idx = -1;
-    return false;
-  }
+  bool add_item(T elem);
+  bool insert_item(int idx, T elem);
+  void remove_item();
+  void remove_item(T elem);
+  void erase_item(int idx);
+  bool find_item(T elem, int& idx);
 
   // operators
   T& operator[](int idx) {
