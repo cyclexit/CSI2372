e9be84e7cb7dfa055a47a2a47ce74766c01596ff
cyclexit <cyclexit@outlook.com>
2022-11-02 21:11:25 -0400

    Add some test cases

--- assignment/2/TestGraph.cpp
+++ assignment/2/TestGraph.cpp
@@ -15,6 +15,12 @@ int main() {
     cout << "Test - get_node_count ... Failed" << endl;
   }
 
+  if (g1.connectivity_type() == 3) {
+    cout << "Test - single vertex strongly connected ... Passed" << endl;
+  } else {
+    cout << "Test - single vertex strongly connected ... Failed" << endl;
+  }
+
   // operator ++
   ++g1;
   if (g1.get_node_count() == Graph::kDefaultNodeCount + 1) {
@@ -23,34 +29,66 @@ int main() {
     cout << "Test - operator++ ... Failed" << endl;
   }
 
-  // add one edge
+  if (g1.connectivity_type() == 0) {
+    cout << "Test - two vertex not connected ... Passed" << endl;
+  } else {
+    cout << "Test - two vertex not connected ... Failed" << endl;
+  }
+
+  // add 1->2
   g1.add_edge(1, 2);
   if (g1.edge_exist(1, 2)) {
-    cout << "Test - edge_exist ... Passed" << endl;
+    cout << "Test - edge_exist12 ... Passed" << endl;
   } else {
     cout << "Test - edge_exist ... Failed" << endl;
   }
   if (!g1.edge_exist(2, 1)) {
-    cout << "Test - not edge_exist ... Passed" << endl;
+    cout << "Test - not edge_exist21 ... Passed" << endl;
   } else {
-    cout << "Test - not edge_exist ... Failed" << endl;
+    cout << "Test - not edge_exist21 ... Failed" << endl;
   }
 
   if (g1.path_exist(1, 2)) {
-    cout << "Test - path_exist ... Passed" << endl;
+    cout << "Test - path_exist12 ... Passed" << endl;
   } else {
-    cout << "Test - path_exist ... Failed" << endl;
+    cout << "Test - path_exist12 ... Failed" << endl;
   }
   if (!g1.path_exist(2, 1)) {
-    cout << "Test - not path_exist ... Passed" << endl;
+    cout << "Test - not path_exist21 ... Passed" << endl;
   } else {
-    cout << "Test - not path_exist ... Failed" << endl;
+    cout << "Test - not path_exist21 ... Failed" << endl;
+  }
+
+  if (g1.connectivity_type() == 2) {
+    cout << "Test - two vertex unilaterally connected ... Passed" << endl;
+  } else {
+    cout << "Test - two vertex unilaterally connected ... Failed" << endl;
   }
 
   if (g1.get_degree(1) == 1 && g1.get_degree(2) == 0) {
     cout << "Test - get_degree ... Passed" << endl;
   } else {
     cout << "Test - get_degree ... Failed" << endl;
+  }
+
+  // add 2->1
+  g1.add_edge(2, 1);
+  if (g1.edge_exist(2, 1)) {
+    cout << "Test - edge_exist21 ... Passed" << endl;
+  } else {
+    cout << "Test - edge_exist21 ... Failed" << endl;
+  }
+
+  if (g1.path_exist(2, 1)) {
+    cout << "Test - path_exist21 ... Passed" << endl;
+  } else {
+    cout << "Test - path_exist21 ... Failed" << endl;
+  }
+
+  if (g1.connectivity_type() == 3) {
+    cout << "Test - two vertex strongly connected ... Passed" << endl;
+  } else {
+    cout << "Test - two vertex strongly connected ... Failed" << endl;
   }
 
   cout << g1 << endl;
