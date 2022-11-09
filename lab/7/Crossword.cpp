64faf7aa70bd8a4e485977c8fb06907d6614e6b5
cyclexit <cyclexit@outlook.com>
2022-11-09 10:49:15 -0500

    Implement the ctors

--- lab/7/Crossword.cpp
+++ lab/7/Crossword.cpp
@@ -1,1 +1,17 @@
 #include "Crossword.h"
+
+Crossword::Crossword() {
+  rows = kDefaultRows;
+  columns = kDefaultColumns;
+  questions.resize(rows);
+  solved_state.resize(rows, std::vector<std::string>(columns, " "));
+  current_state.resize(rows, std::vector<std::string>(columns, " "));
+}
+
+Crossword::Crossword(int n, int m) {
+  rows = n;
+  columns = m;
+  questions.resize(rows);
+  solved_state.resize(rows, std::vector<std::string>(columns, " "));
+  current_state.resize(rows, std::vector<std::string>(columns, " "));
+}
