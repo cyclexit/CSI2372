48161da217c75727a722cb8a8bc0c4ded28c7ff9
cyclexit <cyclexit@outlook.com>
2022-11-15 13:51:52 -0500

    Finish the design of class

--- lab/8/Mapping.h
+++ lab/8/Mapping.h
@@ -6,14 +6,32 @@
 #include <iostream>
 
 template<typename T1, typename T2>
+class Mapping;
+
+template<typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const Mapping<T1, T2>& m);
+
+template<typename T1, typename T2>
 class Mapping {
  public:
+  // ctors and dtors
   Mapping() = default;
   Mapping(const Mapping& other);
   ~Mapping() = default;
- 
+
+  // methods
+  void add_item(T1 k, T2 v);
+  void remove_item(T1 k, T2 v);
+  void clear(T1 k);
+  std::set<T2> find_item(T1 k);
+
+  // operators
+  std::set<T2> operator[](T1 k);
+  Mapping<T1, T2>& operator=(const Mapping<T1, T2>& other);
+  friend std::ostream& operator<<(std::ostream& out, const Mapping<T1, T2>& m);
+
  private:
-  std::map<T1, std::set<T2>> mp;
+  std::map<T1, std::set<T2>> my_map_;
 };
 
 #endif // LAB_8_MAPPING_H_
