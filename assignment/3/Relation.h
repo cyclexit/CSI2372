28d31ca7b5e447a1a0cce3cc6e2e39c1418245ef
cyclexit <cyclexit@outlook.com>
2022-11-15 16:05:29 -0500

    Make the Relation as class template

--- assignment/3/Relation.h
+++ assignment/3/Relation.h
@@ -4,34 +4,45 @@
 #ifndef CSI2372_ASSIGNMENT_3_RELATION_H_
 #define CSI2372_ASSIGNMENT_3_RELATION_H_
 
+#include <iostream>
 #include <set>
 #include <utility>
 
+template<typename T>
+class Relation;
+
+template<typename T>
+std::ostream& operator<< <> (std::ostream& out, const Relation<T>& rel);
+
+template<typename T>
 class Relation {
  public:
   // ctors and dtors
   Relation() = default;
-  Relation(const Relation& other);
+  Relation(const Relation<T>& other);
   ~Relation() = default;
 
   // methods
   size_t cardinality() const;
-  bool add_element(const std::pair<int, int>& r);
-  void remove_element(const std::pair<int, int>& r);
-  bool is_member(const std::pair<int, int>& r) const;
-  bool equal(const Relation& other) const;
+  bool add_element(const std::pair<T, T>& r);
+  void remove_element(const std::pair<T, T>& r);
+  bool is_member(const std::pair<T, T>& r) const;
+  bool equal(const Relation<T>& other) const;
   bool reflexive() const;
   bool irreflexive() const;
   bool symmetric() const;
   bool asymmetric() const;
   bool transitive() const;
   bool is_function() const;
-  Relation inverse() const;
-  Relation combination(const Relation& other) const;
+  Relation<T> inverse() const;
+  Relation<T> combination(const Relation<T>& other) const;
+
+  // operators
+  friend std::ostream& operator<< <> (std::ostream& out, const Relation<T>& rel);
 
  private:
-  std::set<int> elems_;
-  std::set<std::pair<int, int>> relations_;
+  std::set<T> elems_;
+  std::set<std::pair<T, T>> relations_;
 };
 
 #endif // CSI2372_ASSIGNMENT_3_RELATION_H_
