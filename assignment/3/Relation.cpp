423eb6148aad1ac3ee1bd2ba6ae75747f4c64bbe
cyclexit <cyclexit@outlook.com>
2022-11-15 16:15:11 -0500

    Implement Relation class until is_member

--- assignment/3/Relation.cpp
+++ assignment/3/Relation.cpp
@@ -2,3 +2,33 @@
 // Stacy Guo 300157080
 
 #include "Relation.h"
+
+template<typename T>
+Relation<T>::Relation(const Relation<T>& other) {
+  elems_ = other.elems_;
+  relations_ = other.relations_;
+}
+
+template<typename T>
+size_t Relation<T>::cardinality() const {
+  return relations_.size();
+}
+
+template<typename T>
+bool Relation<T>::add_element(const std::pair<T, T>& r) {
+  auto ret = relations_.insert(r);
+  return ret.second;
+}
+
+template<typename T>
+void Relation<T>::remove_element(const std::pair<T, T>& r) {
+  auto itr = relations_.find(r);
+  if (itr != relations_.end()) {
+    relations_.erase(itr);
+  }
+}
+
+template<typename T>
+bool Relation<T>::is_member(const std::pair<T, T>& r) const {
+  return relations_.count(r) > 0;
+}
