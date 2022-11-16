82e6c1b91ca0ff2e59bb77a8407f9d19917d780b
cyclexit <cyclexit@outlook.com>
2022-11-16 16:59:49 -0500

    Use short alias for MappingRelation

--- assignment/3/MappingRelation.h
+++ assignment/3/MappingRelation.h
@@ -14,12 +14,17 @@ class MappingRelation;
 template<typename Domain, typename Range>
 std::ostream& operator<<(std::ostream& out, const MappingRelation<Domain, Range>& mr);
 
+// short alias for the class name
+template<typename Domain, typename Range>
+using MR = MappingRelation<Domain, Range>;
+
 template<typename Domain, typename Range>
 class MappingRelation {
  public:
+
   // ctors and dtors
   MappingRelation() = default;
-  MappingRelation(const MappingRelation<Domain, Range>& other);
+  MappingRelation(const MR<Domain, Range>& other);
   MappingRelation(std::set<Domain> domain, std::set<Range> range);
   ~MappingRelation() = default;
 
@@ -30,16 +35,16 @@ class MappingRelation {
   void remove_element(const std::pair<Domain, Range>& r);
   bool is_member(const std::pair<Domain, Range>& r) const;
   bool is_function() const;
-  MappingRelation<Domain, Range> intersection(const MappingRelation<Domain, Range>& other) const;
+  MR<Domain, Range> intersection(const MR<Domain, Range>& other) const;
   MappingRelation<Range, Domain> inverse() const; // bonus
-  MappingRelation<Domain, Range> combination(const MappingRelation<Domain, Range>& other) const; // bonus
+  MR<Domain, Range> combination(const MR<Domain, Range>& other) const; // bonus
 
   // operators
-  bool operator==(const MappingRelation<Domain, Range>& other) const;
-  MappingRelation<Domain, Range> operator+(const MappingRelation<Domain, Range>& other) const;
-  MappingRelation<Domain, Range> operator-(const MappingRelation<Domain, Range>& other) const;
+  bool operator==(const MR<Domain, Range>& other) const;
+  MR<Domain, Range> operator+(const MR<Domain, Range>& other) const;
+  MR<Domain, Range> operator-(const MR<Domain, Range>& other) const;
   std::set<Range> operator[](Domain d) const;
-  friend std::ostream& operator<< <> (std::ostream& out, const MappingRelation<Domain, Range>& mr);
+  friend std::ostream& operator<< <> (std::ostream& out, const MR<Domain, Range>& mr);
 
  private:
   std::set<Domain> domain_;
