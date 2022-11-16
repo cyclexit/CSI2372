3e24b0fd4912415f629bd775de95ccd802e78ae1
cyclexit <cyclexit@outlook.com>
2022-11-16 15:47:04 -0500

    Class ends

--- assignment/3/MappingRelation.h
+++ assignment/3/MappingRelation.h
@@ -4,6 +4,17 @@
 #ifndef CSI2372_ASSIGNMENT_3_MAPPING_RELATION_H_
 #define CSI2372_ASSIGNMENT_3_MAPPING_RELATION_H_
 
+template<typename Domain, typename Range>
+class MappingRelation;
 
+template<typename Domain, typename Range>
+class MappingRelation {
+ public:
+  MappingRelation() = default;
+  MappingRelation(const MappingRelation<Domain, Range>& other);
+  ~MappingRelation() = default;
+
+ private:
+};
 
 #endif // CSI2372_ASSIGNMENT_3_MAPPING_RELATION_H_
