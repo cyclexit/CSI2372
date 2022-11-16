ac4b86807ad140fbf3a4cfdf9431c52c53f95952
cyclexit <cyclexit@outlook.com>
2022-11-16 16:21:12 -0500

    Make some progress

--- assignment/3/MappingRelation.h
+++ assignment/3/MappingRelation.h
@@ -4,17 +4,30 @@
 #ifndef CSI2372_ASSIGNMENT_3_MAPPING_RELATION_H_
 #define CSI2372_ASSIGNMENT_3_MAPPING_RELATION_H_
 
+#include <iostream>
+#include <set>
+#include <utility>
+
 template<typename Domain, typename Range>
 class MappingRelation;
 
 template<typename Domain, typename Range>
 class MappingRelation {
  public:
+  // ctors and dtors
   MappingRelation() = default;
   MappingRelation(const MappingRelation<Domain, Range>& other);
+  MappingRelation(std::set<Domain> domain, std::set<Range> range);
   ~MappingRelation() = default;
 
+  // methods
+  bool add_to_domain(Domain d);
+  bool add_to_rangeg(Range r);
+
  private:
+  std::set<Domain> domain_;
+  std::set<Range> range_;
+  std::set<std::pair<Domain, Range>> relations_;
 };
 
 #endif // CSI2372_ASSIGNMENT_3_MAPPING_RELATION_H_
