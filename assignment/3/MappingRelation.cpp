a1146b74360daf027d0213bbd9c1b90077436719
cyclexit <cyclexit@outlook.com>
2022-11-17 09:15:31 -0500

    Implement all ctors and dtors

--- assignment/3/MappingRelation.cpp
+++ assignment/3/MappingRelation.cpp
@@ -4,6 +4,15 @@
 #include "MappingRelation.h"
 
 template<typename Domain, typename Range>
-MR<Domain, Range>::MappingRelation(const MR<Domain, Range>& other) {
-  
+MappingRelation<Domain, Range>::MappingRelation(const MappingRelation<Domain, Range>& other) {
+  domain_ = other.domain_;
+  range_ = other.range_;
+  relations_ = other.relations_;
+}
+
+template<typename Domain, typename Range>
+MappingRelation<Domain, Range>::MappingRelation(std::set<Domain> domain,
+                                                std::set<Range> range) {
+  domain_ = domain;
+  range_ = range;
 }
