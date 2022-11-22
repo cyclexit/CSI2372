dd61aaf7e5de99d5a9a7f2f68517f23ba250f2ac
cyclexit <cyclexit@outlook.com>
2022-11-22 13:34:29 -0500

    Init lab9

---
+++ lab/9/lab9.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+static const string kInputFileName = "CSI2372_final.info";
+static const string kOutputFileName = "CSI2372_Final.info";
+
+int main() {
+  return 0;
+}
