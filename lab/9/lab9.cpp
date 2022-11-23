d6b8e865ec8f6f9f9ba411cd8eb6b17f343b90fd
cyclexit <cyclexit@outlook.com>
2022-11-23 14:59:59 -0500

    Read the file

--- lab/9/lab9.cpp
+++ lab/9/lab9.cpp
@@ -4,9 +4,17 @@
 
 using namespace std;
 
-static const string kInputFileName = "CSI2372.info";
-static const string kOutputFileName = "CSI2372_Final.info";
+static const string kInputFilePath = "./CSI2372.info";
+static const string kOutputFilePath = "./CSI2372_Final.info";
 
 int main() {
+  ifstream fin;
+  fin.open(kInputFilePath, ios::binary);
+  cout << fin.is_open() << endl; // debug
+  string tmp;
+  while (getline(fin, tmp)) {
+    cout << tmp << endl;
+  }
+  fin.close();
   return 0;
 }
