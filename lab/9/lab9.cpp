c97f08b3ddfd3bc54bb84606541c7ea3b66ba5a3
cyclexit <cyclexit@outlook.com>
2022-11-23 19:53:15 -0500

    Make some progress

--- lab/9/lab9.cpp
+++ lab/9/lab9.cpp
@@ -14,14 +14,21 @@ struct Student {
 
   string first_name;
   string last_name;
-  long long student_id;
+  int student_id;
   double labs_mark;
   vector<double> assignment_marks;
   double term_test_mark;
   double midterm_mark;
   double final_mark;
 
-  Student() {assignment_marks.resize(kTotalAssignments);}
+  Student() {assignment_marks.resize(kTotalAssignments, 0.0);}
+
+  static size_t byte_len_without_names() {
+    return sizeof(decltype(student_id)) + sizeof(decltype(labs_mark))
+            + sizeof(decltype(assignment_marks)::value_type) * kTotalAssignments
+            + sizeof(decltype(term_test_mark)) + sizeof(decltype(midterm_mark))
+            + sizeof(decltype(final_mark));
+  }
 
   // friend istream& operator>>(istream& in, Student& s) {
   //   in >> s.first_name >> s.last_name >> s.student_id;
@@ -37,11 +44,32 @@ struct Student {
 int main() {
   ifstream fin;
   fin.open(kInputFilePath, ios::binary);
+  if (!fin.is_open()) {
+    cout << "File not found!" << endl;
+    return 1;
+  }
   string data;
   while (getline(fin, data)) {
     cout << data << endl;
   }
   fin.close();
+
+  cout << Student::byte_len_without_names() << endl;
+
   stringstream ss(data);
+  cout << "Tokens: " << endl;
+  int cnt = 0;
+  while (getline(ss, data, ';')) {
+    cout << ++cnt << ": " << data << endl << "\tsize = " << data.size() << endl;
+    if (cnt == 3 || (cnt > 3 && cnt % 3 == 2)) {
+      stringstream tmp_ss(data);
+
+      int id;
+      tmp_ss.read((char *)&id, sizeof(decltype(id)));
+      cout << "\t" << "id = " << id << endl;
+
+      cout << endl;
+    }
+  }
   return 0;
 }
