c737f46513c2d85306fa54748a097ef22e09b5a7
cyclexit <cyclexit@outlook.com>
2022-11-23 20:42:45 -0500

    Got all student names

--- lab/9/lab9.cpp
+++ lab/9/lab9.cpp
@@ -1,75 +1,107 @@
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <sstream>
 #include <vector>
 
-using namespace std;
-
-static const string kInputFilePath = "./CSI2372.info";
-static const string kOutputFilePath = "./CSI2372_Final.info";
+static const std::string kInputFilePath = "./CSI2372.info";
+static const std::string kOutputFilePath = "./CSI2372_Final.info";
 
 struct Student {
   static constexpr int kTotalAssignments = 5;
 
-  string first_name;
-  string last_name;
+  std::string first_name;
+  std::string last_name;
+  // payload begins
   int student_id;
   double labs_mark;
-  vector<double> assignment_marks;
+  std::vector<double> assignment_marks;
   double term_test_mark;
   double midterm_mark;
   double final_mark;
+  // payload ends
 
   Student() {assignment_marks.resize(kTotalAssignments, 0.0);}
 
-  static size_t byte_len_without_names() {
+  static size_t payload_byte_len() {
     return sizeof(decltype(student_id)) + sizeof(decltype(labs_mark))
             + sizeof(decltype(assignment_marks)::value_type) * kTotalAssignments
             + sizeof(decltype(term_test_mark)) + sizeof(decltype(midterm_mark))
             + sizeof(decltype(final_mark));
   }
-
-  // friend istream& operator>>(istream& in, Student& s) {
-  //   in >> s.first_name >> s.last_name >> s.student_id;
-  //   in >> s.labs_mark;
-  //   for (int i = 0; i < Student::kTotalAssignments; ++i) {
-  //     in >> s.assignment_marks[i];
-  //   }
-  //   in >> s.term_test_mark >> s.midterm_mark >> s.final_mark;
-  //   return in;
-  // }
 };
 
 int main() {
-  ifstream fin;
-  fin.open(kInputFilePath, ios::binary);
+  std::ifstream fin;
+  // read the input file
+  fin.open(kInputFilePath, std::ios::binary);
   if (!fin.is_open()) {
-    cout << "File not found!" << endl;
+    std::cout << "File not found!" << std::endl;
     return 1;
   }
-  string data;
+  std::string data;
   while (getline(fin, data)) {
-    cout << data << endl;
+    std::cout << data << std::endl;
   }
   fin.close();
 
-  cout << Student::byte_len_without_names() << endl;
+  // decode the data
+  std::regex names_pattern("\\w+;\\w+");
+  std::sregex_iterator names_begin(
+    data.begin(),
+    data.end(),
+    names_pattern
+  );
+  std::sregex_iterator names_end;
+  std::sregex_token_iterator payload_begin(
+    data.begin(),
+    data.end(),
+    names_pattern,
+    -1
+  );
+  std::sregex_token_iterator payload_end;
 
-  stringstream ss(data);
-  cout << "Tokens: " << endl;
-  int cnt = 0;
-  while (getline(ss, data, ';')) {
-    cout << ++cnt << ": " << data << endl << "\tsize = " << data.size() << endl;
-    if (cnt == 3 || (cnt > 3 && cnt % 3 == 2)) {
-      stringstream tmp_ss(data);
+  std::vector<Student> all_students;
+  for (auto itr = names_begin; itr != names_end; ++itr) {
+    std::smatch names_match = *itr;
+    std::stringstream ss(names_match.str());
 
-      int id;
-      tmp_ss.read((char *)&id, sizeof(decltype(id)));
-      cout << "\t" << "id = " << id << endl;
-
-      cout << endl;
+    Student cur;
+    bool is_first_name = true;
+    std::string tmp;
+    while (getline(ss, tmp, ';')) {
+      if (is_first_name) {
+        is_first_name = false;
+        cur.first_name = tmp;
+      } else {
+        cur.last_name = tmp;
+      }
     }
+
+    all_students.push_back(cur);
   }
+  std::cout << all_students.size() << std::endl;
+  for (auto s : all_students) {
+    std::cout << s.first_name << " " << s.last_name << std::endl;
+  }
+
+  // std::cout << Student::payload_byte_len() << std::endl;
+
+  // stringstream ss(data);
+  // std::cout << "Tokens: " << std::endl;
+  // int cnt = 0;
+  // while (getline(ss, data, ';')) {
+  //   std::cout << ++cnt << ": " << data << std::endl << "\tsize = " << data.size() << std::endl;
+  //   if (cnt == 3 || (cnt > 3 && cnt % 3 == 2)) {
+  //     stringstream tmp_ss(data);
+
+  //     int id;
+  //     tmp_ss.read((char *)&id, sizeof(decltype(id)));
+  //     std::cout << "\t" << "id = " << id << std::endl;
+
+  //     std::cout << std::endl;
+  //   }
+  // }
   return 0;
 }
