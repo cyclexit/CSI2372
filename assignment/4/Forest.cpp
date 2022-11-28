7bc99fb8e98296ff282d651385a21603015c904b
cyclexit <cyclexit@outlook.com>
2022-11-28 16:56:00 -0500

    Finish the implementation of Forest::add_edge

--- assignment/4/Forest.cpp
+++ assignment/4/Forest.cpp
@@ -5,7 +5,10 @@ bool Forest::add_edge(int u, int v) {
     printf("Error: Node %d and/or %d not exist.\n", u, v);
     return false;
   }
-  // TODO: check the cycle between u and v
+  if (!edge_exist(u, v) && path_exist(u, v)) {
+    printf("Error: Add the edge (%d, %d) will form a cycle!\n", u, v);
+    return false;
+  }
   edges_[u].push_back(v);
   edges_[v].push_back(u);
   return true;
