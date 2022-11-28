1d090bbeb2b38a9baff9ebe1cde4bc1549fed618
cyclexit <cyclexit@outlook.com>
2022-11-28 17:40:14 -0500

    Init src files for Tree class

---
+++ assignment/4/TestTree.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#include "Tree.h"
+
+using namespace std;
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T> vec) {
+  for (int i = 0; i < vec.size(); ++i) {
+    if (i > 0) out << ", ";
+    out << vec[i];
+  }
+  return out;
+}
+
+int main() {
+  return 0;
+}
---
+++ assignment/4/Tree.h
@@ -0,0 +1,6 @@
+#ifndef CSI2372_ASSIGNMENT_4_TREE_H_
+#define CSI2372_ASSIGNMENT_4_TREE_H_
+
+#include "Forest.h"
+
+#endif // CSI2372_ASSIGNMENT_4_TREE_H_
