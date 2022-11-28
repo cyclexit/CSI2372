f9dcd1315f717ee16fa3acdb3ced8c4fbe08fba1
cyclexit <cyclexit@outlook.com>
2022-11-28 16:40:05 -0500

    Finish Forest.h

--- assignment/4/Forest.h
+++ assignment/4/Forest.h
@@ -4,7 +4,12 @@
 #include "Graph.h"
 
 class Forest : public Graph {
+ public:
+  // methods
+  bool add_edge(int u, int v) override;
 
+  // operators
+  friend std::ostream& operator<<(std::ostream& out, const Forest& forest);
 };
 
 #endif // CSI2372_ASSIGNMENT_4_FOREST_H_
