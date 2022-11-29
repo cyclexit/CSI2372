fd2943ea463ee753f72b8de10ed19f55fadfaa6f
cyclexit <cyclexit@outlook.com>
2022-11-29 14:29:14 -0500

    Implement the default constructor

--- assignment/4/Tree.cpp
+++ assignment/4/Tree.cpp
@@ -1,1 +1,11 @@
 #include "Tree.h"
+
+Tree::Tree() {
+  edges_.resize(kDefaultNodeCount + 1);
+  set_root(1);
+}
+
+bool Tree::set_root(int node) {
+  if (!is_node_valid(node)) return false;
+  return root = node;
+}
