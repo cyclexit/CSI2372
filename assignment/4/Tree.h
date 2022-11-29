5b288891eea20c8262658a3d4af9d0039fdebcbf
cyclexit <cyclexit@outlook.com>
2022-11-28 20:45:22 -0500

    Finish Tree.h

--- assignment/4/Tree.h
+++ assignment/4/Tree.h
@@ -3,4 +3,23 @@
 
 #include "Forest.h"
 
+class Tree : public Forest {
+ public:
+  // ctors and dtors
+  Tree();
+
+  // methods
+  bool set_root(int node);
+
+  // operators
+  Graph& operator++() override;
+  Graph operator++(int) override;
+  Graph& operator--() override;
+  Graph operator--(int) override;
+  friend std::ostream& operator<<(std::ostream& out, const Tree& tree);
+
+ protected:
+  int root;
+};
+
 #endif // CSI2372_ASSIGNMENT_4_TREE_H_
