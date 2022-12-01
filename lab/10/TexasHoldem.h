19213ac6d61f6c743cb6da137f2674238eb472d8
cyclexit <cyclexit@outlook.com>
2022-11-30 22:22:59 -0500

    Finish the design of TexasHoldem

--- lab/10/TexasHoldem.h
+++ lab/10/TexasHoldem.h
@@ -4,7 +4,12 @@
 #include "Poker.h"
 
 class TexasHoldem : public Poker {
+ public:
+  TexasHoldem();
+  TexasHoldem(int total_players);
 
+  void deal();
+  std::vector<std::string> hands();
 };
 
 #endif // LAB_10_TEXAS_HOLDEN_H_
