1c6aebaecf072e025c657c4c64ffc2b35608c7df
cyclexit <cyclexit@outlook.com>
2022-11-30 21:02:07 -0500

    IsFlush and IsStraight

--- lab/10/Poker.cpp
+++ lab/10/Poker.cpp
@@ -42,28 +42,7 @@ bool Poker::add_to_table() {
 }
 
 bool Poker::IsStraightFlush(const std::vector<PokerCard>& hand) {
-  if (!is_hand_legal(hand)) return false;
-
-  std::set<std::string> suit_set;
-  std::set<int> rank_set;
-  for (const auto& card : hand) {
-    suit_set.insert(card.suit);
-    rank_set.insert(kPokerCardRanks.at(card.rank));
-  }
-
-  if (suit_set.size() != 1) return false;
-  if (rank_set.size() != kHandSize) return false;
-
-  // special check for royal flush
-  std::vector<int> ranks(rank_set.begin(), rank_set.end()); // already sorted
-  int sum = std::accumulate(ranks.begin(), ranks.end(), 0);
-  if (sum == 47 && ranks[0] == 1 && ranks[kHandSize - 1] == 13) return true;
-
-  // normal check for other flushes
-  for (int i = 1; i < ranks.size(); ++i) {
-    if (ranks[i] != ranks[i - 1] + 1) return false;
-  }
-  return true;
+  return IsStraight(hand) && IsFlush(hand);
 }
 
 bool Poker::IsFourofaKind(const std::vector<PokerCard>& hand) {
@@ -81,6 +60,7 @@ bool Poker::IsFourofaKind(const std::vector<PokerCard>& hand) {
 
 bool Poker::IsFullHouse(const std::vector<PokerCard>& hand) {
   if (!is_hand_legal(hand)) return false;
+
   std::map<std::string, int> rank_counter;
   for (const auto& card : hand) {
     ++rank_counter[card.rank];
@@ -94,10 +74,33 @@ bool Poker::IsFullHouse(const std::vector<PokerCard>& hand) {
 
 bool Poker::IsFlush(const std::vector<PokerCard>& hand) {
   if (!is_hand_legal(hand)) return false;
+
+  std::set<std::string> suit_set;
+  for (const auto& card : hand) {
+    suit_set.insert(card.suit);
+  }
+  return suit_set.size() == 1;
 }
 
 bool Poker::IsStraight(const std::vector<PokerCard>& hand) {
   if (!is_hand_legal(hand)) return false;
+
+  std::set<int> rank_set;
+  for (const auto& card : hand) {
+    rank_set.insert(kPokerCardRanks.at(card.rank));
+  }
+  if (rank_set.size() != kHandSize) return false;
+
+  // special check for royal flush
+  std::vector<int> ranks(rank_set.begin(), rank_set.end()); // already sorted
+  int sum = std::accumulate(ranks.begin(), ranks.end(), 0);
+  if (sum == 47 && ranks[0] == 1 && ranks[kHandSize - 1] == 13) return true;
+
+  // normal check for other flushes
+  for (int i = 1; i < ranks.size(); ++i) {
+    if (ranks[i] != ranks[i - 1] + 1) return false;
+  }
+  return true;
 }
 
 bool Poker::IsThreeofaKind(const std::vector<PokerCard>& hand) {
