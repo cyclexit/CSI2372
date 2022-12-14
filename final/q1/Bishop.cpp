d108770d79d8a0eeed53aec934e4bf43d6a89370
cyclexit <cyclexit@outlook.com>
2022-12-14 10:49:29 -0500

    Implement Bishop

---
+++ final/q1/Bishop.cpp
@@ -0,0 +1,29 @@
+#include "Bishop.h"
+
+Bishop::Bishop(int clr, int r, char c) : Piece(clr, r, c) {}
+
+bool Bishop::is_move_valid(int r, char c) {
+  if (!in_chessboard(r, c)) return false;
+
+  for (int i = 0; i < kMaxColumn; ++i) {
+    // diagonal
+    if ((r == row + i && c == column + i)
+        || (r == row - i && c == column - i)
+        || (r == row + i && c == column - i)
+        || (r == row - i && c == column + i)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool Bishop::move(int r, char c) {
+  if (is_move_valid(r, c)) {
+    row = r;
+    column = c;
+    return true;
+  }
+
+  return false;
+}
---
+++ final/q1/Bishop.h
@@ -0,0 +1,16 @@
+#ifndef FINAL_BISHOP_H_
+#define FINAL_BISHOP_H_
+
+#include "Piece.h"
+
+class Bishop : public Piece {
+ public:
+  Bishop() = default;
+  Bishop(int clr, int r, char c);
+  ~Bishop() = default;
+
+  bool is_move_valid(int r, char c) override;
+  bool move(int r, char c) override;
+};
+
+#endif // FINAL_BISHOP_H_
