6f48d4368624286fbaeb2b212848825a3e030cb4
cyclexit <cyclexit@outlook.com>
2022-12-14 12:06:11 -0500

    Implement ctors

--- final/q2/Monochrome.cpp
+++ final/q2/Monochrome.cpp
@@ -1,1 +1,30 @@
 #include "Monochrome.h"
+
+Monochrome::Monochrome() : Image() {
+  intensity %= kMaxIntensity;
+  for (int h = 0; h < height; ++h) {
+    for (int w = 0; w < width; ++w) {
+      pixels[h][w] = intensity;
+    }
+  }
+}
+
+Monochrome::Monochrome(int w, int h, int inten) : Image(w, h, inten) {
+  intensity %= kMaxIntensity;
+  for (int h = 0; h < height; ++h) {
+    for (int w = 0; w < width; ++w) {
+      pixels[h][w] = intensity;
+    }
+  }
+}
+
+Monochrome::Monochrome(const Monochrome& other) : Image(other) {
+  intensity %= kMaxIntensity;
+  for (int h = 0; h < height; ++h) {
+    for (int w = 0; w < width; ++w) {
+      pixels[h][w] = intensity;
+    }
+  }
+}
+
+
