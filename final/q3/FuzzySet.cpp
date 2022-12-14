04431bccd60942aea00e08575ebf8edd019db423
cyclexit <cyclexit@outlook.com>
2022-12-14 12:48:37 -0500

    Implement operator +

--- final/q3/FuzzySet.cpp
+++ final/q3/FuzzySet.cpp
@@ -14,8 +14,11 @@ FuzzySet<T> FuzzySet<T>::intersection(const FuzzySet<T>& other) {
   FuzzySet<T> res;
 
   for (pair<T, double> p : fuzzy) {
+    if (p.second == 0) continue;
     if (other.fuzzy.count(p.first) == 1) {
-      res.fuzzy[p.first] = min(p.second, other.fuzzy[p.first]);
+      if (other.fuzzy[p.first] > 0) {
+        res.fuzzy[p.first] = min(p.second, other.fuzzy[p.first]);
+      }
     }
   }
 
@@ -24,7 +27,17 @@ FuzzySet<T> FuzzySet<T>::intersection(const FuzzySet<T>& other) {
 
 template<typename T>
 FuzzySet<T> FuzzySet<T>::operator+(const FuzzySet<T>& other) const {
+  FuzzySet<T> res;
 
+  for (pair<T, double> p : fuzzy) {
+    res.fuzzy[p.first] = p.second;
+  }
+
+  for (pair<T, double> p : other.fuzzy) {
+    res.fuzzy[p.first] = max(res.fuzzy[p.first], p.second);
+  }
+
+  return res;
 }
 
 template<typename T>
