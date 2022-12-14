0809ebf88e080fd20e4ff459e4f8c7c2d8617b14
cyclexit <cyclexit@outlook.com>
2022-12-13 22:36:28 -0500

    Fix polymorphism errors

--- lecture/Triangles/RightAngledIsosceles.cpp
+++ lecture/Triangles/RightAngledIsosceles.cpp
@@ -7,14 +7,14 @@
 
 using namespace std;
 
-RightAngledIsosceles::RightAngledIsosceles()
+RightAngledIsosceles::RightAngledIsosceles() : Isosceles(1.0, 1.0, sqrt(1.0)) , RightAngledTriangle(1.0, 1.0)
 {
 	//Triangle t = Triangle(1.0, 1.0, sqrt(2.0));
-	RightAngledTriangle rt = RightAngledTriangle(1.0, 1.0);
+	// RightAngledTriangle rt = RightAngledTriangle(1.0, 1.0);
 }
 
-RightAngledIsosceles::RightAngledIsosceles(double x)
+RightAngledIsosceles::RightAngledIsosceles(double x) : Isosceles(x, x, sqrt(2.0) * x), RightAngledTriangle(x, x)
 {
 	//Triangle t = Triangle(x, x, sqrt(2.0) * x);
-	RightAngledTriangle rt = RightAngledTriangle(x, x);
+	// RightAngledTriangle rt = RightAngledTriangle(x, x);
 }
